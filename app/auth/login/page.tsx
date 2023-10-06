2e75f8f65876e5d78067676dfdda2d37e8c36b28
David Chavez <depch47@gmail.com>
2023-10-05 20:24:15 -0500

    Add google btn and working on protecting routes

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import { Title } from "@mantine/core"
+import SignInForm from "./components/SignIn"
+
+
+const LoginPage = () => {
+
+
+  return (
+    <>
+      <Title>Login</Title>
+      <SignInForm />
+    </>
+  )
+}
+
+export default LoginPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
-'use client'
+import Link from "next/link"
 
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 
-export default function Home() {
 
-  const router = useRouter()
 
-  useEffect(() => {
-    router.push("/login")
-  })
+const Home = () => {
 
+  // add some logo or landing page later
+  // then a Link to the sign in form
+
+  const signInPageURL = "/auth/login"
   return (
-    <></>
+    <>
+      <h1>Welcome back</h1>
+      <p>Please log in</p>
+      <Link href={signInPageURL}>Go to sign in</Link>
+    </>
   )
 }
+
+export default Home
