7631f45427f2876bdb40e4047f6216f08b6409d0
David Chavez <depch47@gmail.com>
2023-10-06 22:18:41 -0500

    Add server actions

--- app/home/settings/components/general/GeneralSettings.tsx
+++ app/home/settings/components/general/GeneralSettings.tsx
@@ -1,56 +1,17 @@
-"use client"
 
-import { Box, TextInput } from "@mantine/core"
-import { useForm } from "@mantine/form"
-import { useLocalStorage } from "@mantine/hooks"
-import NamesList from "./NamesList"
+import ParticipantForm from "./ParticipantForm"
 
 
-interface FormValues {
-  names: string
-}
+
 
 
 const GeneralSetting = () => {
-  const [participants, setParticipants] = useLocalStorage<string[]>({ key: "participants", defaultValue: [] })
-  const form = useForm<FormValues>({
-    initialValues: {
-      names: '',
-    },
 
-    // validate: {
-    //   names: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    // },
-  });
-
-  const handleSubmit = (values: FormValues) => {
-    const names = values.names.split(",").map((n) => n.trim()).filter((n) => n)
-    setParticipants((prev) => [...prev, ...names])
-    form.reset()
-
-  }
-
-  const removeFromList = (id: string ) => {
-    setParticipants((prev) => prev.filter((_, i) => String(i) !== id))
-  }
-
-  
   return (
     <>
-      <Box style={{ display: "flex", justifyContent: "center" }}>
-        <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
-          <TextInput
-            label="Participants"
-            description="Enter a comma separated list of names. "
-            placeholder="i.e. Maria, Juan, Jose"
-            {...form.getInputProps('names')}
-          />
-        </form>
+      <ParticipantForm />
 
-      </Box>
-      <Box style={{ display: "flex", justifyContent: "center" }}>
-        <NamesList list={participants || []} removeItem={removeFromList}/>
-      </Box>
+      {/* <NamesList list={[]} removeItem={removeFromList} /> */}
     </>
   )
 }
