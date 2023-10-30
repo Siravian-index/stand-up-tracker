ace1c63009938ad6abc8b1607d558c1e8164ef97
David Chavez <depch47@gmail.com>
2023-10-30 16:55:54 -0500

    Add simple styles to navbar

--- app/home/components/Navbar.tsx
+++ app/home/components/Navbar.tsx
@@ -1,9 +1,9 @@
 
 "use client"
 
-import { ActionIcon } from '@mantine/core';
+import { ActionIcon, Flex } from '@mantine/core';
 import Link from 'next/link';
-import { IconPhoto, IconSettings } from '@tabler/icons-react';
+import { IconLogout, IconPhoto, IconSettings } from '@tabler/icons-react';
 import { signOut } from 'next-auth/react';
 
 const Navbar = () => {
@@ -20,22 +20,31 @@ const Navbar = () => {
   }
 
   return (
-    <nav>
-      {/* TODO add navbar */}
-      <Link href="/home/daily">
-        <ActionIcon variant="filled" color="gray" size="sm" aria-label="Settings">
-          <IconPhoto style={{ width: '70%', height: '70%' }} stroke={1.5} />
+    <Flex
+      justify="center"
+      align="center"
+      mt="sm"
+    >
+      <Flex
+      gap="sm"
+      >
+        {/* TODO add navbar */}
+        <Link href="/home/daily">
+          <ActionIcon variant="filled" color="gray" size="md" aria-label="Settings">
+            <IconPhoto stroke={1.5} />
+          </ActionIcon>
+        </Link>
+        <Link href="/home/settings">
+          <ActionIcon variant="filled" color="gray" size="md" aria-label="Settings">
+            <IconSettings stroke={1.5} />
+          </ActionIcon>
+        </Link>
+        <ActionIcon variant="filled" color="gray" size="md" aria-label="Logout" onClick={handleSignOut}>
+          <IconLogout stroke={1.5} />
         </ActionIcon>
-      </Link>
-      <Link href="/home/settings">
-        <ActionIcon variant="filled" color="gray" size="sm" aria-label="Settings">
-          <IconSettings style={{ width: '70%', height: '70%' }} stroke={1.5} />
-        </ActionIcon>
-      </Link>
-      <ActionIcon variant="filled" color="gray" size="sm" aria-label="Settings" onClick={handleSignOut}>
-        <IconSettings style={{ width: '70%', height: '70%' }} stroke={1.5} />
-      </ActionIcon>
-    </nav>
+      </Flex>
+    </Flex>
+
   )
 }
 
